a219b35a8a25695cb4a06a4eefe7f358168761be
Deglier Amorim <Deglier Amorim>
2016-01-28 17:43:15 -0200

    internal changes

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,14 +1,24 @@
-var parallaxHeight = $('.parallax').height();
+var nice            =   false;
+var parallaxHeight  =   $('.parallax').height();
+var parallaxWidth   =   $('.parallax').width();
+var mobileWidth    =   '480';
+$(document).ready(
+  function() {
+    nice = $("html").niceScroll();
+  }
+);
 
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
-  if(wScroll <= parallaxHeight) {
-    $('.logo').css({
-      'transform' : 'translate(0px, ' + wScroll /5 +'%)'
-    });
-    $('.background').css({
-      'transform' : 'translate(0px, ' + wScroll /20 +'%)'
-    });
+  if(parallaxWidth > mobileWidth) {
+    if(wScroll <= parallaxHeight) {
+      $('.logo').css({
+        'transform' : 'translate(0px, ' + wScroll /3 +'%)'
+      });
+      $('.background').css({
+        'transform' : 'translate(0px, ' + wScroll /20 +'%)'
+      });
+    }
   }
 });
