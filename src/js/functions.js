9c96c26eaef759504b59aa1d577223a63146b0a6
Deglier Amorim <Deglier Amorim>
2016-01-29 16:28:23 -0200

    refatoring js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,24 +1,69 @@
-var nice            =   false;
-var parallaxHeight  =   $('.parallax').height();
-var parallaxWidth   =   $('.parallax').width();
-var mobileWidth    =   '480';
-$(document).ready(
-  function() {
+(function($){
+$.fn.scrollParallax = function(settings) {
+    return this.each(function() {
+      var config = {
+          'speed': 20,
+          'mobile': false,
+          'mobileWidth': 480
+      };
+      if (settings){$.extend(config, settings);}
+
+      var parallax      =   $(this);
+      var height        =   parallax.height();
+      var width         =   parallax.width();
+      var mobileWidth   =   config.mobileWidth;
+      var speed         =   config.speed;
+
+      $(window).scroll(function(){
+        var wScroll = $(this).scrollTop();
+
+        if(width > mobileWidth) {
+          if(wScroll <= height) {
+            parallax.css({
+              'transform' : 'translate(0px, ' + wScroll / speed +'%)'
+            });
+          }
+        }
+    });
+ };
+})(jQuery);
+/*
+(function($){
+    $.scrollParallax = function(selector, settings){
+        var config = {
+            'speed': 20,
+            'mobile': false,
+            'mobileWidth': 480
+        };
+        if (settings){$.extend(config, settings);}
+
+        var parallax      =   $(selector);
+        var height        =   parallax.height();
+        var width         =   parallax.width();
+        var mobileWidth   =   config.mobileWidth;
+        var speed         =   config.speed;
+
+        $(window).scroll(function(){
+          var wScroll = $(this).scrollTop();
+
+          if(width > mobileWidth) {
+            if(wScroll <= height) {
+              parallax.css({
+                'transform' : 'translate(0px, ' + wScroll / speed +'%)'
+              });
+            }
+          }
+        });
+        console.log(mobileWidth);
+
+        return this;
+    };
+})(jQuery);
+*/
+$(document).ready(function() {
     nice = $("html").niceScroll();
+    //$.scrollParallax(".background",{'speed': 10});
+    //$.scrollParallax(".logo",{'speed': 20});
+    logo = $(".logo").scrollParallax();
   }
 );
-
-$(window).scroll(function(){
-  var wScroll = $(this).scrollTop();
-
-  if(parallaxWidth > mobileWidth) {
-    if(wScroll <= parallaxHeight) {
-      $('.logo').css({
-        'transform' : 'translate(0px, ' + wScroll /10 +'%)'
-      });
-      $('.background').css({
-        'transform' : 'translate(0px, ' + wScroll /20 +'%)'
-      });
-    }
-  }
-});
