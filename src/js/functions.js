72e1ebe6aeb56ce708fd374531ea5be418410d91
Deglier Amorim <Deglier Amorim>
2016-02-01 14:47:24 -0200

    using pure js for parallax fx

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,69 +1,25 @@
-(function($){
-$.fn.scrollParallax = function(settings) {
-    return this.each(function() {
-      var config = {
-          'speed': 20,
-          'mobile': false,
-          'mobileWidth': 480
-      };
-      if (settings){$.extend(config, settings);}
+function parallax(){
+  var el			=		document.querySelectorAll(".parallax-item");
+  var scroll	=		window.pageYOffset;
+  for (var i = 0; i < el.length; i++) {
+    if(scroll < el[i].offsetHeight){
+      console.log("continua: "+ i);
+      el[i].style.transform = 'translate(0%,' + scroll/((i+1)*10) + '%)';
+    }else{
+      console.log("nao continua :)");
+    }
+    //console.log('elemento: "' + el[i] + '"');
+    //console.log(el[i].style.transform = 'translate(0%,' + scroll/((i+10)*10) + '%)');
+  }
+  //fundo.style.transform = 'translate(0%,' + scroll/speed + '%)';
 
-      var parallax      =   $(this);
-      var height        =   parallax.height();
-      var width         =   parallax.width();
-      var mobileWidth   =   config.mobileWidth;
-      var speed         =   config.speed;
+}
+	document.addEventListener("scroll", parallax);
 
-      $(window).scroll(function(){
-        var wScroll = $(this).scrollTop();
-
-        if(width > mobileWidth) {
-          if(wScroll <= height) {
-            parallax.css({
-              'transform' : 'translate(0px, ' + wScroll / speed +'%)'
-            });
-          }
-        }
-    });
- };
-})(jQuery);
-/*
-(function($){
-    $.scrollParallax = function(selector, settings){
-        var config = {
-            'speed': 20,
-            'mobile': false,
-            'mobileWidth': 480
-        };
-        if (settings){$.extend(config, settings);}
-
-        var parallax      =   $(selector);
-        var height        =   parallax.height();
-        var width         =   parallax.width();
-        var mobileWidth   =   config.mobileWidth;
-        var speed         =   config.speed;
-
-        $(window).scroll(function(){
-          var wScroll = $(this).scrollTop();
-
-          if(width > mobileWidth) {
-            if(wScroll <= height) {
-              parallax.css({
-                'transform' : 'translate(0px, ' + wScroll / speed +'%)'
-              });
-            }
-          }
-        });
-        console.log(mobileWidth);
-
-        return this;
-    };
-})(jQuery);
-*/
 $(document).ready(function() {
     nice = $("html").niceScroll();
     //$.scrollParallax(".background",{'speed': 10});
     //$.scrollParallax(".logo",{'speed': 20});
-    logo = $(".logo").scrollParallax();
+    //logo = $(".logo").scrollParallax();
   }
 );
