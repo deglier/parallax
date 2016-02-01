5ffedb4e713ec71ca221813302d12d0201f26dfa
Deglier Amorim <Deglier Amorim>
2016-02-01 14:55:12 -0200

    using pure js for parallax fx

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,25 +1,26 @@
+
+/* Calls jquery functions */
+$(document).ready(function() {
+  nice = $("html").niceScroll();
+}
+);
+
+
+
+/**
+* @author: Deglier Amorim
+**/
+/* parallax effect */
 function parallax(){
   var el			=		document.querySelectorAll(".parallax-item");
   var scroll	=		window.pageYOffset;
   for (var i = 0; i < el.length; i++) {
     if(scroll < el[i].offsetHeight){
-      console.log("continua: "+ i);
       el[i].style.transform = 'translate(0%,' + scroll/((i+1)*10) + '%)';
-    }else{
-      console.log("nao continua :)");
     }
-    //console.log('elemento: "' + el[i] + '"');
-    //console.log(el[i].style.transform = 'translate(0%,' + scroll/((i+10)*10) + '%)');
   }
-  //fundo.style.transform = 'translate(0%,' + scroll/speed + '%)';
 
 }
-	document.addEventListener("scroll", parallax);
 
-$(document).ready(function() {
-    nice = $("html").niceScroll();
-    //$.scrollParallax(".background",{'speed': 10});
-    //$.scrollParallax(".logo",{'speed': 20});
-    //logo = $(".logo").scrollParallax();
-  }
-);
+/* create event for parallax */
+document.addEventListener("scroll", parallax);
